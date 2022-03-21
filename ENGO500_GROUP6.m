ace12c3ae1e2c01ce4ad1decbfdcc49def0340df
Caitlyn Maida <90862467+caitlynmaida@users.noreply.github.com>
2022-03-20 21:05:52 -0600

    Add files via upload

--- ENGO500_GROUP6.m
+++ ENGO500_GROUP6.m
@@ -6,30 +6,34 @@ clc;
 %% Plotting Parsed Data
 M = readtable('INSPVAX.csv');
 
+% time = M.seconds(:);
 % column = M.azimuthsigma(:);
 % name = "Azimuth Error - 30 Minute Driving Test";
 % xlabel = "Duration [s]";
 % ylabel = "Azimuth Error [degrees]";
 % 
-% output(column,name,xlabel,ylabel);
+% output(time,column,name,xlabel,ylabel);
 
+% time = M.seconds(:);
 % column = M.lat(:);
 % name = "Latitude - 30 Minute Driving Test";
 % xlabel = "Duration [s]";
 % ylabel = "Latitude [degrees]";
 % 
-% output(column,name,xlabel,ylabel);
+% output(time,column,name,xlabel,ylabel);
 
+% time = M.seconds(:);
 % column = M.lon(:);
 % name = "Longitude - 30 Minute Driving Test";
 % xlabel = "Duration [s]";
 % ylabel = "Longitude [degrees]";
 % 
-% output(column,name,xlabel,ylabel);
+% output(time,column,name,xlabel,ylabel);
 
-column = M.hgt(:);
-name = "Height Above Mean Sea Level - 30 Minute Driving Test";
-xlabel = "Duration [s]";
-ylabel = "Height [m]";
-
-output(column,name,xlabel,ylabel);
+% time = M.seconds(:);
+% column = M.hgt(:);
+% name = "Height Above Mean Sea Level - 30 Minute Driving Test";
+% xlabel = "Duration [s]";
+% ylabel = "Height [m]";
+% 
+% output(time,column,name,xlabel,ylabel);
--- output.m
+++ output.m
@@ -2,17 +2,34 @@
 % given data
 
 % inputs:
+% t - time
 % c - column needed for plot
 % n - plot name
 % x - xlabel
 % y - ylabel
 
-function output(c,n,x,y)
+function output(t,c,n,x,y)
 
-c = rmoutliers(c);
+t1 = t(1,1);
+
+for i = 1:height(t)
+    t(i,1) = t(i,1) - t1;
+end
+
+%%% - comment this section out to keep outliers
+% TF = isoutlier(c);
+% 
+% for j = 1:height(TF)
+%     if TF(j,1) == 1
+%         t(j) = [];
+%     end
+% end
+% 
+% c = rmoutliers(c);
+%%% - comment this section out to keep outliers
 
 figure();
-plot(c, 'b-', 'LineWidth', 2);
+plot(t, c, 'b-', 'LineWidth', 2);
 grid on;
 title(n);
 xlabel(x);
